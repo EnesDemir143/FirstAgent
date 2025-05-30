6c8f96a1e0498a886e2ebe29c686123154d2567d
EnesDemir143 <enesd41@outlook.com>
2025-05-30 22:53:24 +0300

    llm node tamamlandı.

--- internal/Agent/AgentFlow.py
+++ internal/Agent/AgentFlow.py
@@ -2,9 +2,13 @@ from langchain_openai import ChatOpenAI
 from langgraph.graph import START, END, StateGraph
 from .memory import AgentState
 from dotenv import load_dotenv
+from Node.LLMNode import llm_node
 
 load_dotenv()
 
 graph = StateGraph(AgentState)
+
+graph.add_node('llm_node', llm_node)
+
 
 
--- internal/Agent/Node/LLMNode.py
+++ internal/Agent/Node/LLMNode.py
@@ -1,8 +1,13 @@
 from langchain_openai import ChatOpenAI
 from memory import AgentState
-from lana
+from langchain_core.messages import SystemMessage
 
 def llm_node(llm: ChatOpenAI, state: AgentState) -> AgentState:
     '''This node is responsible for invoke llm.
     Then After the llm call depend on the response call the end or again use tool.'''
-    system_message = 
+
+    system_message = SystemMessage(content='You are a very helpful assistant for read some documents.Then you get inference on them.But just about this documents.You are get the inference and if you need some research on internet.Then you ouse some Tools.')
+
+    response = llm.invoke([system_message] + state['messages'])
+    state['messages'].append(response)
+    return state
